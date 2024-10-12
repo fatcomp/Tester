5fe80f177fb64507f85250565a7673080c92e1c5
fatcomp <fatmaa@qu.edu.qa>
2024-10-12 16:11:40 +0300

    Create Test2.java

---
+++ Test/src/Test2.java
@@ -0,0 +1,12 @@
+
+public class Test2 {
+	public Test2() {
+		for(int i =0;i<10;i++)
+			System.out.print(i);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Test2();
+	}
+
+}
